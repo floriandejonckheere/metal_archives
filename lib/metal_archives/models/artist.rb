1ae4a692ad7cb2a24f7e0d04a44179eff2134ffe
Florian Dejonckheere <florian@floriandejonckheere.be>
2016-03-29 18:40:50 +0200

    Artist model documentation

--- lib/metal_archives/models/artist.rb
+++ lib/metal_archives/models/artist.rb
@@ -7,27 +7,97 @@ module MetalArchives
   # Represents an artist (person or group)
   #
   class Artist < BaseModel
-    property :id
+    ##
+    # :attr_reader: id
+    #
+    # Returns +Integer+
+    #
+    property :id, :type => Integer
+
+    ##
+    # :attr_reader: name
+    #
+    # Returns +String+
+    #
     property :name
-    property :aliases,                        :multiple => true
-    property :country,      :type => ISO3166::Country
+
+    ##
+    # :attr_reader: aliases
+    #
+    # Returns +Array+ of +String+
+    #
+    property :aliases, :multiple => true
+
+    ##
+    # :attr_reader: country
+    #
+    # Returns +ISO3166::Country+
+    #
+    property :country, :type => ISO3166::Country
+
+    ##
+    # :attr_reader: location
+    #
+    # Returns +String+
+    #
     property :location
-    property :date_formed,  :type => Date
-    property :date_active,  :type => MetalArchives::Range,   :multiple => true
-    property :genres,                         :multiple => true
-    property :themes,                         :multiple => true
-    property :label,        :type => MetalArchives::Label
+
+    ##
+    # :attr_reader: date_formed
+    #
+    # Returns +Date+
+    #
+    property :date_formed, :type => Date
+
+    ##
+    # :attr_reader: date_active
+    #
+    # Returns +Array+ of rdoc-ref:Range
+    #
+    property :date_active, :type => MetalArchives::Range, :multiple => true
+
+    ##
+    # :attr_reader: genres
+    #
+    # Returns +Array+ of +String+
+    #
+    property :genres, :multiple => true
+
+    ##
+    # :attr_reader: lyrical_themes
+    #
+    # Returns +Array+ of +String+
+    #
+    property :lyrical_themes, :multiple => true
+
+    ##
+    # :attr_reader: label
+    #
+    # Returns rdoc-ref:Label
+    #
+    property :label, :type => MetalArchives::Label
+
+    ##
+    # :attr_reader: comment
+    #
+    # Returns +String+
+    #
     property :comment
 
+    ##
+    # :attr_reader: status
+    #
+    # Returns +:active+, +:split_up+, +:on_hold+, +:unknown+, +:changed_name+ or +:disputed+
+    #
     enum :status,       :values => [:active, :split_up, :on_hold, :unknown, :changed_name, :disputed]
 
     # TODO: n-way associations
 
     class << self
       ##
-      # Search for an artist.
+      # Search by name.
       #
-      # Returns an +Array+ with rdoc-ref:Artist instances.
+      # Returns +Array+ of rdoc-ref:Artist
       #
       def search(name)
         results = []
@@ -35,9 +105,9 @@ module MetalArchives
       end
 
       ##
-      # Find an artist by name and id.
+      # Find by name and id.
       #
-      # Returns an rdoc-ref:Artist instance.
+      # Returns rdoc-ref:Artist
       #
       def find_by_name(name, id)
         client.find_resource(
