6e52b13d62f37138c2a881d0cb8a7f91d05e0e1e
Florian Dejonckheere <florian@floriandejonckheere.be>
2016-03-29 21:18:15 +0200

    Remove RSpec files

--- spec/config_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe MetalArchives.config do
-  it 'has a valid configuration' do
-    expect(subject.app_name).not_to be_nil
-    expect(subject.app_version).not_to be_nil
-    expect(subject.app_contact).not_to be_nil
-  end
-end
--- spec/parsers/artist_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe MetalArchives::Parsers::Artist do
-
-  it 'provides an endpoint' do
-    expect(MetalArchives::Parsers::Artist.find_endpoint({})).to be_a(String)
-  end
-
-end
