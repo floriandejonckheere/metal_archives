d2621c0c8a618ddf8fbcc2d92223ca90ac5456de
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-05-21 20:38:37 +0200

    Add LRUCache spec

---
+++ spec/utils/lru_cache_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+RSpec.describe MetalArchives::LRUCache do
+  let(:cache) { MetalArchives::LRUCache.new 3 }
+
+  it 'stores and retrieves objects' do
+    cache[:key] = 'MyString'
+
+    expect(cache[:key]).to eq 'MyString'
+  end
+
+  it 'clears cache' do
+    cache[:key] = 'MyString'
+    cache.clear
+
+    expect(cache[:key]).to be_nil
+  end
+
+  it 'peeks' do
+    expect(cache).not_to include :key
+
+    cache[:key] = 'MyString'
+
+    expect(cache).to include :key
+  end
+
+  it 'implements LRU caching' do
+    cache[:a] = 'one'
+    cache[:b] = 'two'
+    cache[:c] = 'three'
+
+    expect(cache.instance_variable_get '@size').to eq 3
+
+    cache[:d] = 'four'
+
+    expect(cache.instance_variable_get '@size').to eq 3
+
+    expect(cache[:b]).to eq 'two'
+    expect(cache[:c]).to eq 'three'
+    expect(cache[:d]).to eq 'four'
+    expect(cache[:a]).to be_nil
+  end
+end
