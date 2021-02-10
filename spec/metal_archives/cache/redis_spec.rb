3bdb423e1d32b687c0bea013313c1848730c849e
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-02-10 17:45:06 +0100

    Add CI REDIS_URL

--- spec/metal_archives/cache/redis_spec.rb
+++ spec/metal_archives/cache/redis_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe MetalArchives::Cache::Redis do
   before do
     MetalArchives.configure do |c|
       c.cache_strategy = "redis"
-      c.cache_options = { url: "redis://localhost:6379" }
+      c.cache_options = { url: ENV.fetch("REDIS_URL", "redis://localhost:6379") }
     end
 
     MetalArchives.cache.clear
