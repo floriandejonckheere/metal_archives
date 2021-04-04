4cc9d71636defe58664bd26222d4d4b35cc4cf44
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-04-04 15:19:17 +0200

    Initializable spec: use integer ids

--- spec/support/shared_examples/initializable.rb
+++ spec/support/shared_examples/initializable.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples "it is initializable" do
-  subject(:model) { described_class.new(id: "my_id") }
+  subject(:model) { described_class.new(id: 1) }
 
-  it { is_expected.to have_attributes id: "my_id" }
-  it { is_expected.not_to eq described_class.new(id: "not_my_id") }
+  it { is_expected.to have_attributes id: 1 }
+  it { is_expected.not_to eq described_class.new(id: 2) }
 
   it "raises when no id is passed on initialization" do
     expect { described_class.new }.to raise_error ArgumentError
