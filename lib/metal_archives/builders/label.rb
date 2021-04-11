ff1c4de7d09e0f2f15e164712a01f9f0c004bbd7
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-04-11 16:02:44 +0200

    Replace label parser with builder

---
+++ lib/metal_archives/builders/label.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+module MetalArchives
+  module Builders
+    ##
+    # Label builder
+    #
+    class Label < Base
+      attr_reader :label
+
+      def initialize(label)
+        super()
+
+        @label = label
+      end
+
+      def build!
+        # Attributes
+        label.set(**attributes(
+          http.get("/labels/id/#{label.id}"),
+        ))
+
+        # TODO: parent label, sub-labels
+        # TODO: roster
+        # TODO: releases
+        # TODO: links
+        # TODO: notes
+      end
+
+      private
+
+      def attributes(response)
+        doc = Nokogiri::HTML(response.body.to_s)
+
+        info = doc.at("#label_info")
+
+        # TODO: remove "N/A"
+        {
+          name: info.at(".label_name").content,
+          contact: doc.css("#label_contact a").map { |c| { title: c.content, content: c["href"] } },
+          address: info.at("dl dt:contains('Address') ~ dd")&.content,
+          country: ISO3166::Country.find_country_by_name(info.at("dl dt:contains('Country') ~ dd")&.content),
+          phone: info.at("dl dt:contains('Phone') ~ dd")&.content,
+          status: info.at("dl dt:contains('Status') ~ dd")&.content&.parameterize(separator: "_")&.to_sym,
+          specializations: genres(info.at("dl dt:contains('Styles') ~ dd")&.content),
+          date_founded: date(info.at("dl dt:contains('Founding date') ~ dd")&.content),
+          online_shopping: info.at("dl dt:contains('Online shopping') ~ dd")&.content&.include?("Yes"),
+        }
+      end
+    end
+  end
+end
---
+++ spec/metal_archives/models/label_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+RSpec.describe MetalArchives::Label do
+  subject(:label) { described_class.new(id: id) }
+
+  describe "AFM Records" do
+    let(:id) { 17 }
+
+    around { |example| VCR.use_cassette("labels/afm", &example) }
+
+    it "has properties" do
+      expect(label.name).to eq "AFM Records"
+      expect(label.address).to eq "Agathe-Lasch-Weg 2 22605 Hamburg"
+      expect(label.country).to eq ISO3166::Country["DE"]
+      expect(label.phone).to eq "+49 (0)40 675 09 788 - 0"
+      expect(label.specializations).to match_array %w(Heavy Power Gothic Thrash)
+      expect(label.date_founded).to eq Date.new(1995)
+      expect(label.status).to eq :active
+      expect(label.online_shopping).to eq true
+    end
+
+    it "has contacts" do
+      expect(label.contact).to include(
+        { title: "AFM Records Website", content: /www.afm-records.de/ },
+        { title: "ed/sdrocer-mfa//ofni", content: nil },
+      )
+    end
+  end
+end
